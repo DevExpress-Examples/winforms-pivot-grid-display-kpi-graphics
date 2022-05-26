7c2e8a166b6d540d7cd2a7f2395e7334ceb7b82a
DevExpressExampleBot <fakeEmail>
2022-05-26 12:57:40 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApp_RegularDataSourceKPI/Form1.Designer.vb
+++ VB/WindowsFormsApp_RegularDataSourceKPI/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WindowsFormsApp_RegularDataSourceKPI
-    Partial Public Class Form1
+Namespace WindowsFormsApp_RegularDataSourceKPI
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,37 +12,37 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim DataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.salesPersonBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-            Me.nwindDataSet = New nwindDataSet()
+            Me.nwindDataSet = New WindowsFormsApp_RegularDataSourceKPI.nwindDataSet()
             Me.fieldCategoryName = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.fieldExtendedPrice = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.fieldSalesPerson = New DevExpress.XtraPivotGrid.PivotGridField()
-            Me.salesPersonTableAdapter = New nwindDataSetTableAdapters.SalesPersonTableAdapter()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.salesPersonBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.salesPersonTableAdapter = New WindowsFormsApp_RegularDataSourceKPI.nwindDataSetTableAdapters.SalesPersonTableAdapter()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.salesPersonBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
+            ' 
+            ' pivotGridControl1
+            ' 
             Me.pivotGridControl1.DataSource = Me.salesPersonBindingSource
             Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldCategoryName, Me.fieldExtendedPrice, Me.fieldSalesPerson})
@@ -49,72 +51,76 @@
             Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
             Me.pivotGridControl1.Size = New System.Drawing.Size(383, 292)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'salesPersonBindingSource
-            '
+            ' 
+            ' salesPersonBindingSource
+            ' 
             Me.salesPersonBindingSource.DataMember = "SalesPerson"
             Me.salesPersonBindingSource.DataSource = Me.nwindDataSet
-            '
-            'nwindDataSet
-            '
+            ' 
+            ' nwindDataSet
+            ' 
             Me.nwindDataSet.DataSetName = "nwindDataSet"
             Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-            '
-            'fieldCategoryName
-            '
+            ' 
+            ' fieldCategoryName
+            ' 
             Me.fieldCategoryName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
             Me.fieldCategoryName.AreaIndex = 0
             Me.fieldCategoryName.Caption = "Category Name"
-            DataSourceColumnBinding1.ColumnName = "CategoryName"
-            Me.fieldCategoryName.DataBinding = DataSourceColumnBinding1
+            dataSourceColumnBinding1.ColumnName = "CategoryName"
+            Me.fieldCategoryName.DataBinding = dataSourceColumnBinding1
             Me.fieldCategoryName.Name = "fieldCategoryName"
             Me.fieldCategoryName.Width = 178
-            '
-            'fieldExtendedPrice
-            '
+            ' 
+            ' fieldExtendedPrice
+            ' 
             Me.fieldExtendedPrice.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.fieldExtendedPrice.AreaIndex = 0
             Me.fieldExtendedPrice.Caption = "Extended Price"
-            DataSourceColumnBinding2.ColumnName = "Extended Price"
-            Me.fieldExtendedPrice.DataBinding = DataSourceColumnBinding2
+            dataSourceColumnBinding2.ColumnName = "Extended Price"
+            Me.fieldExtendedPrice.DataBinding = dataSourceColumnBinding2
             Me.fieldExtendedPrice.Name = "fieldExtendedPrice"
-            '
-            'fieldSalesPerson
-            '
+            ' 
+            ' fieldSalesPerson
+            ' 
             Me.fieldSalesPerson.AreaIndex = 0
             Me.fieldSalesPerson.Caption = "Sales Person"
-            DataSourceColumnBinding3.ColumnName = "Sales Person"
-            Me.fieldSalesPerson.DataBinding = DataSourceColumnBinding3
+            dataSourceColumnBinding3.ColumnName = "Sales Person"
+            Me.fieldSalesPerson.DataBinding = dataSourceColumnBinding3
             Me.fieldSalesPerson.Name = "fieldSalesPerson"
-            '
-            'salesPersonTableAdapter
-            '
+            ' 
+            ' salesPersonTableAdapter
+            ' 
             Me.salesPersonTableAdapter.ClearBeforeFill = True
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(383, 292)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.salesPersonBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.salesPersonBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
-
         Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-        Private nwindDataSet As nwindDataSet
+
+        Private nwindDataSet As WindowsFormsApp_RegularDataSourceKPI.nwindDataSet
+
         Private salesPersonBindingSource As System.Windows.Forms.BindingSource
-        Private salesPersonTableAdapter As nwindDataSetTableAdapters.SalesPersonTableAdapter
+
+        Private salesPersonTableAdapter As WindowsFormsApp_RegularDataSourceKPI.nwindDataSetTableAdapters.SalesPersonTableAdapter
+
         Private fieldCategoryName As DevExpress.XtraPivotGrid.PivotGridField
+
         Private fieldExtendedPrice As DevExpress.XtraPivotGrid.PivotGridField
+
         Private fieldSalesPerson As DevExpress.XtraPivotGrid.PivotGridField
     End Class
 End Namespace
-
--- VB/WindowsFormsApp_RegularDataSourceKPI/Form1.vb
+++ VB/WindowsFormsApp_RegularDataSourceKPI/Form1.vb
@@ -1,33 +1,28 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraPivotGrid
 
 Namespace WindowsFormsApp_RegularDataSourceKPI
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Binds the pivot grid to data.
-            Me.salesPersonTableAdapter.Fill(Me.nwindDataSet.SalesPerson)
-
+            salesPersonTableAdapter.Fill(nwindDataSet.SalesPerson)
             ' Creates a new unbound "Status" field to show KPI values.
-            Dim unboundField As PivotGridField = pivotGridControl1.Fields.Add("Status", _
-                                                                              PivotArea.DataArea)
+            Dim unboundField As PivotGridField = pivotGridControl1.Fields.Add()
+            unboundField.Area = PivotArea.DataArea
             ' Sets a column's unbound type and specifies an unbound expression.
-            unboundField.DataBinding = New ExpressionDataBinding(
-                String.Format("(Iif(Sum([{0}])<100000,-1,Iif(Sum([{0}])<150000,0,1)))",
-                fieldExtendedPrice.ExpressionFieldName))
-
+            unboundField.DataBinding = New ExpressionDataBinding(String.Format("(Iif(Sum([{0}])<100000,-1,Iif(Sum([{0}])<150000,0,1)))", fieldExtendedPrice.ExpressionFieldName))
             ' Sets the Data Header Area within which the "Status" Field can be positioned.
-            unboundField.AllowedAreas = DevExpress.XtraPivotGrid.PivotGridAllowedAreas.DataArea
-
+            unboundField.AllowedAreas = PivotGridAllowedAreas.DataArea
             ' Specifies a graphic set used to indicate KPI values.
             unboundField.KPIGraphic = PivotKPIGraphic.Faces
-
         End Sub
     End Class
 End Namespace
