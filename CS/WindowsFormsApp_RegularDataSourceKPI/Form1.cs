7f3b08c202194017a050faddaca9375a69f92eb4
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-10-05 18:08:25 +0400

    Update Form1.cs

--- CS/WindowsFormsApp_RegularDataSourceKPI/Form1.cs
+++ CS/WindowsFormsApp_RegularDataSourceKPI/Form1.cs
@@ -10,20 +10,16 @@ namespace WindowsFormsApp_RegularDataSourceKPI {
         private void Form1_Load(object sender, EventArgs e) {
             // Binds the Pivot Grid to data.
             this.salesPersonTableAdapter.Fill(this.nwindDataSet.SalesPerson);
-
             // Creates a new "Status" field to show KPI values.
             PivotGridField KPIField = pivotGridControl1.Fields.Add();
             KPIField.Area = PivotArea.DataArea;
             KPIField.Caption = "Status";
-
             // Sets a column's data binding and specifies an expression.
             KPIField.DataBinding = new ExpressionDataBinding(
                 string.Format("(Iif(Sum([{0}])<100000,-1,Iif(Sum([{0}])<150000,0,1)))", 
                 fieldExtendedPrice.ExpressionFieldName));
-
             // Sets the Data Header Area within which the "Status" Field can be positioned.
             KPIField.AllowedAreas = DevExpress.XtraPivotGrid.PivotGridAllowedAreas.DataArea;
-
             // Specifies a graphic set used to indicate KPI values.
             KPIField.KPIGraphic = PivotKPIGraphic.Faces;
         }
